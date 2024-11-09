f40ad4eb7477ef5e15da0001d922e8d0e5ba7ecc
Richard Morris <rich@singsurf.org>
2024-11-09 14:28:12 +0000

    add colour generation

--- Triangle.py
+++ Triangle.py
@@ -1,20 +1,19 @@
 import random
 
 class Triangle:
-    def __init__(self, edge_length, start_x, start_y, depth, colours):
+    def __init__(self, edge_length, start_x, start_y, depth, colourgen):
 
         self.edge_length = edge_length
         self.start_x = start_x
         self.start_y = start_y
         self.depth = depth
-        self.colours = colours
+        self.colourgen = colourgen
     
     def __triangle(self, turtle, length, depth):
         if depth == 0:
             return
         # improve flexibility by not hard coding array size
-        colour = self.colours[random.randint(0,len(self.colours)-1)]
-        turtle.color(colour)
+        turtle.color(self.colourgen.next())
 
         for x in range(depth):
             for i in range(3):
@@ -29,3 +28,32 @@ class Triangle:
         turtle.goto(self.start_x,self.start_y)
         turtle.pendown()
         self.__triangle(turtle,self.edge_length,self.depth)
+
+class ColourGenerator:
+    
+    def next(self) -> str:
+        pass
+
+class SingleColourGenerator(ColourGenerator):
+    def __init__(self, colour):
+        self.colour = colour
+
+    def next(self) -> str:
+        return self.colour
+    
+class SequenceColourGenerator(ColourGenerator):
+    def __init__(self, colours):
+        self.colours = colours
+        self.index = 0
+
+    def next(self) -> str:
+        colour = self.colours[self.index]
+        self.index = (self.index + 1) % len(self.colours)
+        return colour
+    
+class RandomColourGenerator(ColourGenerator):
+    def __init__(self, colours):
+        self.colours = colours
+
+    def next(self) -> str:
+        return random.choice(self.colours)
--- triangle_main.py
+++ triangle_main.py
@@ -1,21 +1,19 @@
 from turtle import Turtle
-from Triangle import Triangle
+from Triangle import *
 
 bob = Turtle()
 bob.getscreen().setup(width=600, height=600, startx=0, starty=0)
 
 colours = ["blue","red","magenta","black","yellow"]
 
-start_x = -250
-start_y = -250
-start_length = 500
+Triangle(100, -250, 150, 2, SequenceColourGenerator(["pink","green"])).draw(bob)
 
-tri = Triangle(500, -250, -250, 1, colours[1:2])
+
+tri = Triangle(500, -250, -250, 6, RandomColourGenerator(colours))
 tri.draw(bob)
 
-tri2 = Triangle(250, -250, -250, 1, colours[2:3])
+tri2 = Triangle(250, -250, -250, 1, SingleColourGenerator("green"))
 tri2.draw(bob)
 
-Triangle(125, -250, -250, 2, colours[3:4]).draw(bob)
 
 bob.getscreen().mainloop()
